547896e0a380163ace909ce377655c1425131b48
Rumen Manolov <rrumenov13@ub.edu>
2010-01-14 00:00:00 +0000

    version 0.2

--- R/RcmdrPlugin.SLC.R
+++ R/RcmdrPlugin.SLC.R
@@ -1,28 +1,40 @@
 # Some Rcmdr dialogs for the SLC package
 
-.First.lib <- function(libname, pkgname){
-    if (!interactive()) return()
-    Rcmdr <- options()$Rcmdr
-    plugins <- Rcmdr$plugins
-    if ((!pkgname %in% plugins) && !getRcmdr("autoRestart")) {
-        Rcmdr$plugins <- c(plugins, pkgname)
-        options(Rcmdr=Rcmdr)
+.onAttach <- function(libname, pkgname){
+  if (!interactive()) return()
+  Rcmdr <- options()$Rcmdr
+  plugins <- Rcmdr$plugins
+  if (!pkgname %in% plugins) {
+    Rcmdr$plugins <- c(plugins, pkgname)
+    options(Rcmdr=Rcmdr)
+    if("package:Rcmdr" %in% search()) {
+      if(!getRcmdr("autoRestart")) {
         closeCommander(ask=FALSE, ask.save=TRUE)
         Commander()
-        }
+      }
     }
+    else {
+      Commander()
+    }
+  }
+}
+
+if (getRversion() >= '2.15.1') globalVariables(c('top','namefile','datafile','tkbutton',
+                                                 'buttonsFrame','length.baseline'))
 
 Rcmdr.slcestimates <- function(){
 require(SLC)
-require(tcltk)
 
 getfile <- function() {
-
-        command <- "tclvalue(tkgetOpenFile(filetypes='{{Text files} {.txt}} 
+  command <- "tclvalue(tkgetOpenFile(filetypes='{{Text files} {.txt}} 
                 {{Data files} {.dat}} {{All files} *}'))"
-        assign("namefile", justDoIt(command), envir=.GlobalEnv)
-        if (namefile == "") return();
-        assign("datafile", justDoIt("scan(namefile)"), envir=.GlobalEnv)
+  gassign("namefile",justDoIt(command))
+  if (namefile == "") return();
+  result <- justDoIt("data.frame(values=scan(namefile))")
+  if (class(result)[1] != "try-error"){
+    gassign("datafile", result)
+    activeDataSet("datafile")
+  }
 }
 
 initializeDialog(title=gettextRcmdr("Slope and Level Change Estimates"))
@@ -30,38 +42,39 @@ lengthVar <- tclVar("1")
 lengthEntry <- tkentry(top, width="12", textvariable=lengthVar)
 
 onOK <- function(){
-	closeDialog()
+  closeDialog()
 
-        if (is.numeric(datafile) == FALSE){
+        if (is.numeric(datafile[,1]) == FALSE){
            errorCondition(recall=Rcmdr.slcestimates, message="Invalid Data Type: Original data must be numeric.")
             return()
             }
 
-        command <- paste("as.numeric(",tclvalue(lengthVar),")", sep="")
-        assign("length.baseline", justDoIt(command), envir=.GlobalEnv)
+        length.baseline <- as.numeric(tclvalue(lengthVar))
+#        command <- paste("as.numeric(",tclvalue(lengthVar),")", sep="")
+#        gassign("length.baseline", justDoIt(command))
         if ( (is.numeric(length.baseline) == FALSE) | (length.baseline < 1) ) {
             errorCondition(recall=Rcmdr.slcestimates, message="The length of baseline phase must be greater than 0.")
             return()
             }
 	tkfocus(CommanderWindow())
 
-        doItAndPrint("slcestimates(datafile,length.baseline)")      
+        doItAndPrint(paste("slcestimates(datafile$values,",length.baseline,")",sep=''))      
 	tkfocus(CommanderWindow())
 }
 
 OKCancelHelp(helpSubject="slcestimates")
 tkgrid(labelRcmdr(top, text=""))
-tkgrid(labelRcmdr(top, text=gettextRcmdr(" - Original Data should be loaded before OK - "), fg="blue"))
-tkgrid(labelRcmdr(top, text=""))
+tkgrid(labelRcmdr(top, text=gettextRcmdr(" - Original Data should be loaded before OK - "), fg="blue"), sticky="w")
+tkgrid(labelRcmdr(top, text=""), sticky="w")
 tkgrid(tkbutton(top, text="Select Data File", command=getfile))
-tkgrid(labelRcmdr(top, text=""))
-tkgrid(labelRcmdr(top, text=gettextRcmdr(" - Baseline Phase Length Specification - "), fg="blue"),sticky="e")
-tkgrid(labelRcmdr(top, text=""))
+tkgrid(labelRcmdr(top, text=""), sticky="w")
+tkgrid(labelRcmdr(top, text=gettextRcmdr(" - Baseline Phase Length Specification - "), fg="blue"),sticky="w")
+tkgrid(labelRcmdr(top, text=""), sticky="w")
 tkgrid(tklabel(top, text="Baseline Phase Length:"), lengthEntry)
-tkgrid(labelRcmdr(top, text=""))
-tkgrid(buttonsFrame, sticky="e", columnspan=2)
-tkgrid.configure(lengthEntry, sticky="e")
-dialogSuffix(rows=13, columns=2)
+tkgrid(labelRcmdr(top, text=""), sticky="w")
+tkgrid(buttonsFrame, sticky="w", columnspan=2)
+tkgrid.configure(lengthEntry, sticky="w")
+dialogSuffix(rows=10, columns=1)
 }
 
 Rcmdr.help.SLC <- function(){
